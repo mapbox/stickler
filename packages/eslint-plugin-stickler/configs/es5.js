588d6785ca8a1a6dba70d3caef8323332fbff010
David Clark <dclark@mapbox.com>
2018-07-19 15:30:15 -0700

    Fix ES5 for vars and babel-eslint resolution

--- packages/eslint-plugin-stickler/configs/es5.js
+++ packages/eslint-plugin-stickler/configs/es5.js
@@ -7,5 +7,9 @@ module.exports = {
   globals: {
     // We're assuming that there will always be a Promise polyfill.
     Promise: true
+  },
+  rules: {
+    'no-var': 'off',
+    'prefer-const': 'off'
   }
 };
