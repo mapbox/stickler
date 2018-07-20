b4ac28f2629eb86161fcfa44c1172e6ac809e8fb
David Clark <dclark@mapbox.com>
2018-07-19 17:15:07 -0700

    Only one package

--- packages/eslint-plugin-stickler/configs/promise.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  rules: {
-    '@mapbox/stickler/promise/no-return-wrap': 'error',
-    '@mapbox/stickler/promise/param-names': 'error',
-    '@mapbox/stickler/promise/valid-params': 'error'
-  }
-};
--- packages/stickler-core/test/fixtures/invalid-a.js
+++
@@ -1,3 +0,0 @@
-var foo = 'bar';
-
-new Promise((x, y) => {});
